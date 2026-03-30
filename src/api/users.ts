73cd3d2485aee2aea901d5a61b36aef38d8d1d8c
Ky-2103 <167300748+Ky-2103@users.noreply.github.com>
2026-03-30 14:08:56 +0100

    Update users.ts

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,6 +1,7 @@
 import type { AppUser } from '../types/models.js'
 import { apiRequest } from './http.js'
 
+// Fetch all users
 export function listUsers() {
   return apiRequest<AppUser[]>('users')
 }
